01d1427b5b54d0a9052185653b2be32d66fc35b9
asiamohammed <mohamedasia021@gmail.com>
2022-09-09 22:45:12 +0300

    completed assignement

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Add your code here
+
+
+function submitData(userName, email) {
+    debugger
+    var userData = {
+        name: userName,
+        email: email
+    };
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(userData)
+    })
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+        })
+        .catch(error => {
+            document.querySelector('div').innerHTML = error;
+        });
+}
+
+
+
+console.log(submitData("Steve", "steve@steve.com"))
